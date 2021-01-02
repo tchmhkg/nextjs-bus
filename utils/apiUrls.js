b99d56cb57de8ff8e643aa3be787cab5268ebb45
Truman Cheung <>
2021-01-03 01:38:41 +0800

    update api

--- utils/apiUrls.js
+++ utils/apiUrls.js
@@ -1,1 +1,5 @@
 export const getAllStops = 'https://search.kmb.hk/kmbwebsite/Function/FunctionRequest.ashx?action=getallstops'
+
+export const getNearbyRoutes = `https://citymapper.com/api/2/nearby?brand_ids=KMBBus,LWBBus,RMBBus&limit=1&region_id=hk-hongkong&location=` // need coords value in location
+
+export const getRoutesByLocationId = `https://citymapper.com/api/1/departures?headways=1&region_id=hk-hongkong&ids=`
